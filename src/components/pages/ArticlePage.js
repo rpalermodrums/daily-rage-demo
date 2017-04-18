1dab0860b3548ce4cdaa1d1acc3a08942ad76f0f
rpalermodrums <stix250@gmail.com>
2017-04-17 21:12:58 -0400

    react-create-app on Heroku

---
+++ src/components/pages/ArticlePage.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+
+class ArticlePage extends Component {
+  render () {
+    return (
+      <div className="ink-grid">
+        <section className="column-group gutters article">
+          <div className="xlarge-70 large-70 medium-60 small-100 tiny-100">
+            <article>
+              <header>
+                <h1 className="push-left">Article title</h1>
+                <p className="push-right">Published: </p>
+              </header>
+              <summary>But being in a great hurry to resume scolding the man in the purple Shirt, who was waiting for it in the entry, and seeming to hear nothing but the word "clam," Mrs. Hussey hurried towards an open door leading to the kitchen, and bawling out "clam for two," disappeared.</summary>
+              <figure className="ink-image vertical-space">
+                <img src="holder.js/1200x600/auto/ink" alt="" />
+                  <figcaption className="dark over-bottom">
+                    "Because you wear silver shoes and have killed the Wicked Witch. Besides, you have white in your frock, and only witches and sorceresses wear white."
+                  </figcaption>
+              </figure>
+              <p><blockquote>"Queequeg," said I, "do you think that we can make out a supper for us both on one clam?"</blockquote></p>
+              <p>However, a warm savory steam from the kitchen served to belie the apparently cheerless prospect before us. But when that smoking chowder came in, the mystery was delightfully explained. Oh, sweet friends! hearken to me. It was made of small juicy clams, scarcely bigger than hazel nuts, mixed with pounded ship biscuit, and salted pork cut up into little flakes; the whole enriched with butter, and plentifully seasoned with pepper and salt. Our appetites being sharpened by the frosty voyage, and in particular, Queequeg seeing his favourite fishing food before him, and the chowder being surpassingly excellent, we despatched it with great expedition: when leaning back a moment and bethinking me of Mrs. Hussey's clam and cod announcement, I thought I would try a little experiment. Stepping to the kitchen door, I uttered the word "cod" with great emphasis, and resumed my seat. In a few moments the savoury steam came forth again, but with a different flavor, and in good time a fine cod-chowder was placed before us.</p>
+              <footer>
+                <p><small>Creative Commons Attribution-ShareAlike License</small></p>
+              </footer>
+            </article>
+          </div>
+          <section className="xlarge-30 large-30 medium-40 small-100 tiny-100">
+            <h2>Related</h2>
+            <ul className="unstyled">
+              <li className="column-group half-gutters">
+                <div className="all-40 small-50 tiny-50"><img src="holder.js/640x380/auto/ink" alt="" /></div>
+                <div className="all-60 small-50 tiny-50"><p>"Where's them crabs, Hoo-Hoo?" Edwin demanded. "Granser's set upon  having a snack."</p></div>
+              </li>
+              <li className="column-group half-gutters">
+                <div className="all-40 small-50 tiny-50"><img src="holder.js/640x380/auto/ink" alt="" /></div>
+                <div className="all-60 small-50 tiny-50"><p>"Where's them crabs, Hoo-Hoo?" Edwin demanded. "Granser's set upon  having a snack."</p></div>
+              </li>
+              <li className="column-group half-gutters">
+                <div className="all-40 small-50 tiny-50"><img src="holder.js/640x380/auto/ink" alt="" /></div>
+                <div className="all-60 small-50 tiny-50"><p>"Where's them crabs, Hoo-Hoo?" Edwin demanded. "Granser's set upon  having a snack."</p></div>
+              </li>
+              <li className="column-group half-gutters">
+                <div className="all-40 small-50 tiny-50"><img src="holder.js/640x380/auto/ink" alt="" /></div>
+                <div className="all-60 small-50 tiny-50"><p>"Where's them crabs, Hoo-Hoo?" Edwin demanded. "Granser's set upon  having a snack."</p></div>
+              </li>
+              <li className="column-group half-gutters">
+                <div className="all-40 small-50 tiny-50"><img src="holder.js/640x380/auto/ink" alt="" /></div>
+                <div className="all-60 small-50 tiny-50"><p>"Where's them crabs, Hoo-Hoo?" Edwin demanded. "Granser's set upon  having a snack."</p></div>
+              </li>
+            </ul>
+          </section>
+        </section>
+        <section className="column-group gutters">
+          <div className="all-20 small-100 tiny-100">
+            <h3>heading</h3>
+            <img className="half-bottom-space" src="holder.js/1200x600/auto/ink" alt="" />
+              <p>"Red is not the right word," was the reply. "The plague was scarlet.  The whole face and body turned scarlet in an hour's time. Don't I  know? Didn't I see enough of it? And I am telling you it was scarlet  because&mdash;well, because it was scarlet. There is no other word for it."</p>
+          </div>
+          <div className="all-20 small-100 tiny-100">
+            <h3>heading</h3>
+            <img className="half-bottom-space" src="holder.js/1200x600/auto/ink" alt="" />
+              <p>"Red is not the right word," was the reply. "The plague was scarlet.  The whole face and body turned scarlet in an hour's time. Don't I  know? Didn't I see enough of it? And I am telling you it was scarlet  because&mdash;well, because it was scarlet. There is no other word for it."</p>
+          </div>
+          <div className="all-20 small-100 tiny-100">
+            <h3>heading</h3>
+            <img className="half-bottom-space" src="holder.js/1200x600/auto/ink" alt="" />
+              <p>"Red is not the right word," was the reply. "The plague was scarlet.  The whole face and body turned scarlet in an hour's time. Don't I  know? Didn't I see enough of it? And I am telling you it was scarlet  because&mdash;well, because it was scarlet. There is no other word for it."</p>
+          </div>
+          <div className="all-20 small-100 tiny-100">
+            <h3>heading</h3>
+            <img className="half-bottom-space" src="holder.js/1200x600/auto/ink" alt="" />
+              <p>"Red is not the right word," was the reply. "The plague was scarlet.  The whole face and body turned scarlet in an hour's time. Don't I  know? Didn't I see enough of it? And I am telling you it was scarlet  because&mdash;well, because it was scarlet. There is no other word for it."</p>
+          </div>
+          <div className="all-20 small-100 tiny-100">
+            <h3>heading</h3>
+            <img className="half-bottom-space" src="holder.js/1200x600/auto/ink" alt="" />
+              <p>"Red is not the right word," was the reply. "The plague was scarlet.  The whole face and body turned scarlet in an hour's time. Don't I  know? Didn't I see enough of it? And I am telling you it was scarlet  because&mdash;well, because it was scarlet. There is no other word for it."</p>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default ArticlePage
---
+++ src/components/pages/Homepage.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+class Homepage extends Component {
+  render () {
+    return (
+      <div>
+        <section className="pub hide-all show-large show-xlarge xlarge-push-right large-push-right">
+          <div className="column-group gutters">
+            <a href="#" className="all-100 mrec"><img alt='' src="../assets/js/holder.js/300x250/ink/auto/text: MREC PUB" /></a>
+            <div className="all-50"><img alt='' src="../assets/js/holder.js/300x250/ink/auto/text: PUB" /></div>
+            <div className="all-50"><img alt='' src="../assets/js/holder.js/300x250/ink/auto/text: PUB" /></div>
+          </div>
+          <p>
+            "Red is not the right word," was the reply. "The plague was scarlet.  The whole face and body turned scarlet in an hour's time. Don't I  know? Didn't I see enough of it? And I am telling you it was scarlet  because&mdash;well, because it was scarlet. There is no other word for it."
+          </p>
+        </section>
+
+        <section className="highlight">
+          <div className="highlight-wrapper">
+            <div className="column-group gutters">
+              <div className="xlarge-60 large-60 medium-50 small-100 tiny-100">
+                <div className="image">
+                  <img alt='' src="../assets/js/holder.js/600x430/ink/auto/text: highlight" />
+                </div>
+              </div>
+              <div className="xlarge-40 large-40 medium-50 small-100 tiny-100">
+                <h2>heading</h2>
+                <p>"Red is not the right word," was the reply. "The plague was scarlet.  The whole face and body turned scarlet in an hour's time. Don't I  know? Didn't I see enough of it? And I am telling you it was scarlet  because&mdash;well, because it was scarlet. There is no other word for it."</p>
+              </div>
+            </div>
+            <div className="column-group gutters">
+              <div className="all-50 small-100 tiny-100"><img alt='' src="../assets/js/holder.js/600x430/ink/auto/text: another story" /></div>
+              <div className="all-50 small-100 tiny-100"><img alt='' src="../assets/js/holder.js/600x430/ink/auto/text: another story" /></div>
+            </div>
+            <div className="column-group gutters">
+              <div className="all-33 small-100 tiny-100"><img alt='' src="../assets/js/holder.js/600x430/ink/auto/text: another story" /></div>
+              <div className="all-33 small-100 tiny-100"><img alt='' src="../assets/js/holder.js/600x430/ink/auto/text: another story" /></div>
+              <div className="all-33 small-100 tiny-100"><img alt='' src="../assets/js/holder.js/600x430/ink/auto/text: another story" /></div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default Homepage;
